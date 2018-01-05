00db827fd7e383e81895ad14397db623848a268a
Zhenguan Tang <ztang@osisoft.com>
2018-01-04 16:34:50 -0800

    add the badge to count case in email queue

---
+++ background.js
@@ -0,0 +1,18 @@
+function getCaseNumber(){
+	$.getJSON("http://tsdata/api/cases/emailqueue")
+	.done (function(data, textStatus, jqXHR){
+		var caseNumber = 0;
+		for (var i =0; i < data.length; i++)
+		{
+			if (data[i]['status'] == 'New'){
+				caseNumber ++;
+			}
+		}
+		chrome.browserAction.setBadgeText({text: caseNumber.toString()});
+	})
+	.fail(function(jqxHR, textStatus, errorThrown){
+		console.log(errorThrown.toString());
+	})
+}
+
+setInterval(getCaseNumber, 10000);
