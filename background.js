5e9f057a979d9eda25747d0278c79090dfea7eec
Zhenguan Tang <zhenguantang@gmail.com>
2018-01-20 10:43:29 -0800

    add notification for incomming case in background.js

--- background.js
+++ background.js
@@ -6,12 +6,26 @@ function getCaseNumber(){
 		{
 			if (data[i]['status'] == 'New'){
 				caseNumber ++;
+				createNotification(data[i]);
 			}
 		}
 		chrome.browserAction.setBadgeText({text: caseNumber.toString()});
 	})
 	.fail(function(jqxHR, textStatus, errorThrown){
 		console.log(errorThrown.toString());
+	})
+}
+
+function createNotification(caseInfo){
+	var options = {
+		type: 'basic',
+		iconUrl: 'pionemail.jpg',
+		title: 'New Case in Queue!',
+		message: caseInfo['Summary']
+	};
+	
+	chrome.notifications.create(caseInfo['number'], options, function(id){
+		
 	})
 }
 
